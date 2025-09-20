c0ea8f1dd97edf62e3e8438538c0e512bcdffbf6
harith <harith.kv47@gmail.com>
2025-09-20 19:56:38 +0530

    fix from pychram ide

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import os
 from fastapi import FastAPI
-from db import connect_chroma, client  # import connect function and client
 
 app = FastAPI()
 
-# --- Connect to Chroma Cloud at startup ---
-connect_chroma()
 
 @app.get("/")
 def root():
+    a = 5
+    print(f"value ${a}")
     return {"message": "Hello!"}
 
-@app.get("/collections")
-def list_collections():
-    if client:
-        return client.list_collections()  # example usage
-    return {"error": "DB not connected"}
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 5000))
+
+    port = int(os.getenv("PORT", 8000))
     print(f"Running on port {port} with reload")
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
